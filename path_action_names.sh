433ef952508efa7615bd466b9ba2b4a9a8d3fd1b
sn <son.hngy@gmail.com>
2025-10-23 22:56:56 +0300

    change result print

--- path_action_names.sh
+++ path_action_names.sh
@@ -1,11 +1,13 @@
+N_BENCHMARK_LINES=4
 ./planner > planner_output
 if grep -q Success planner_output; then
-    mapfile -t action_id < <(tail -n +6 planner_output)
+    mapfile -t action_id < <(tail -n +$((N_BENCHMARK_LINES+2))  planner_output)
     mapfile -t action_names < <(grep -A 1 'begin_operator' $1 | grep -v 'begin_operator' | grep -v '\-\-')
     rm -rf planner_plan
     for id in "${action_id[@]}"; do
         echo "(${action_names[id]})" >> planner_plan
     done
-    rm -rf planner_output
     cat planner_plan
+    head -n $((N_BENCHMARK_LINES+1)) planner_output | tail -n +2
+    rm -rf planner_output
 fi
